e0df03ede3509def5f833bfc5cb9925cee9bb9dd
Sndubey <sndubey7718@gmail.com>
2025-10-22 16:51:11 +0530

    updated server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config'
 import connectDB from './configs/mongodb.js';
+import { clerkWebhooks } from './controllers/webhooks.js';
 
 const app = express()
 
@@ -9,7 +10,7 @@ app.use(cors())
 
 
 app.get('/',(req,res)=> res.send("api working"))
-
+app.post('/clerk',express.json(),clerkWebhooks)
 
 const PORT = process.env.PORT || 5000
 
